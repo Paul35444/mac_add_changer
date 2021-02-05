83b77c69a2ddd4dfe0b0b54a56e25744dde9851d
Paul St. Amour <paul35444@aol.com>
2021-02-05 09:52:38 -0600

    added calls to... (all hardcoded) -take eth0 interface down -change mac address -bring eth0 interface up to implement changed mac address

--- mac_add_changer/mac_add_changer.py
+++ mac_add_changer/mac_add_changer.py
@@ -3,4 +3,9 @@
 import subprocess
 
 #show ifconfig info and ability to run shell cmds
-subprocess.call("ifconfig", shell=True)
+#take interface down
+subprocess.call("ifconfig eth0 down", shell=True)
+#change mac address
+subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:66", shell=True)
+#bring interface up to enable mac address change
+subprocess.call("ifconfig eth0 up", shell=True)
