c7909ecd4d1f51cfe2b52d494eb9fd70b1e5f137
Paul St. Amour <paul35444@aol.com>
2021-02-05 10:24:35 -0600

    -changed inputs from string to list -removed shell=True because it entered commands as a string; user could run multiple cmds not intended -added list so the initial entry is the cmd and other entries are args instead of running multiple cmds

--- mac_add_changer/mac_add_changer.py
+++ mac_add_changer/mac_add_changer.py
@@ -10,9 +10,9 @@ print("[+] Changing MAC address for " + interface + " to " + new_mac)
 
 #show ifconfig info and ability to run shell cmds
 #take interface down
-subprocess.call("ifconfig " + interface + " down", shell=True)
+subprocess.call(["ifconfig " + interface + " down"])
 #change mac address
-subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
+subprocess.call(["ifconfig " + interface + " hw ether " + new_mac])
 #bring interface up to enable mac address change
-subprocess.call("ifconfig " + interface + " up", shell=True)
+subprocess.call(["ifconfig " + interface + " up"])
 
