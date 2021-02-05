e0ad1525a678204df48b06a878cd11ad892151e6
Paul St. Amour <paul35444@aol.com>
2021-02-05 10:10:33 -0600

    -variable inputs added for network interface and new mac address -user input added -added vaiables to code for shell cmds

--- mac_add_changer/mac_add_changer.py
+++ mac_add_changer/mac_add_changer.py
@@ -2,10 +2,17 @@
 
 import subprocess
 
+interface = input("Network Interface: ")
+new_mac = input("New MAC Address: ")
+
+#get user input from variable above
+print("[+] Changing MAC address for " + interface + " to " + new_mac)
+
 #show ifconfig info and ability to run shell cmds
 #take interface down
-subprocess.call("ifconfig eth0 down", shell=True)
+subprocess.call("ifconfig " + interface + " down", shell=True)
 #change mac address
-subprocess.call("ifconfig eth0 hw ether 00:11:22:33:44:66", shell=True)
+subprocess.call("ifconfig " + interface + " hw ether " + new_mac, shell=True)
 #bring interface up to enable mac address change
-subprocess.call("ifconfig eth0 up", shell=True)
+subprocess.call("ifconfig " + interface + " up", shell=True)
+
