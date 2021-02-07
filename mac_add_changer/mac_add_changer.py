07c1b335ec0eb8f208d983a20ea1b9e16fc37b12
Paul St. Amour <paul35444@aol.com>
2021-02-07 17:27:57 -0600

    -created func to get user inputs -added get_args func below to call to change_mac func

--- mac_add_changer/mac_add_changer.py
+++ mac_add_changer/mac_add_changer.py
@@ -3,6 +3,17 @@
 import subprocess
 import optparse
 
+#func to call to get user inputs/args
+def get_arguments():
+    parser = optparse.OptionParser()
+#allow the user to use -i or --interface and store it under interface var; help option
+    parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
+#allow the user to us -m or --mac and store it to new_mac var; help option
+    parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
+#take options & args from user input
+#changed to return to call everything from above code
+    return parser.parse_args()
+
 #func to call to change mac address
 def change_mac(interface, new_mac):
 #get user input from variable above
@@ -15,14 +26,7 @@ def change_mac(interface, new_mac):
 #bring interface up to enable mac address change
     subprocess.call(["ifconfig", interface, "up"])
 
-parser = optparse.OptionParser()
-
-#allow the user to use -i or --interface and store it under interface var; help option
-parser.add_option("-i", "--interface", dest="interface", help="Interface to change MAC address")
-#allow the user to us -m or --mac and store it to new_mac var; help option
-parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
-
-#take options & args from user input
-(options, arguments) = parser.parse_args()
-
+#get arguments from func above and store in options
+(options, arguments) = get_arguments()
+#call options from get_args func above and implement under change_mac func
 change_mac(options.interface, options.new_mac)
