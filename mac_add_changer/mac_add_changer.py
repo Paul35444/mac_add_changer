94392ce8d3c858560ae0a2c1f9e14e77f5f129ec
Paul St. Amour <paul35444@aol.com>
2021-02-07 17:08:04 -0600

    -created func to change mac address -call func at end of program

--- mac_add_changer/mac_add_changer.py
+++ mac_add_changer/mac_add_changer.py
@@ -3,6 +3,18 @@
 import subprocess
 import optparse
 
+#func to call to change mac address
+def change_mac(interface, new_mac):
+#get user input from variable above
+    print("[+] Changing MAC address for " + interface + " to " + new_mac)
+#show ifconfig info and ability to run shell cmds
+#take interface down
+    subprocess.call(["ifconfig", interface, "down"])
+#change mac address
+    subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
+#bring interface up to enable mac address change
+    subprocess.call(["ifconfig", interface, "up"])
+
 parser = optparse.OptionParser()
 
 #allow the user to use -i or --interface and store it under interface var; help option
@@ -13,17 +25,8 @@ parser.add_option("-m", "--mac", dest="new_mac", help="New MAC address")
 #take options & args from user input
 (options, arguments) = parser.parse_args()
 
-interface = options.interface
-new_mac = options.new_mac
+#REMOVED beacuse they are called in the func below
+#interface = options.interface
+#new_mac = options.new_mac
 
-#get user input from variable above
-print("[+] Changing MAC address for " + interface + " to " + new_mac)
-
-#show ifconfig info and ability to run shell cmds
-#take interface down
-subprocess.call(["ifconfig", interface, "down"])
-#change mac address
-subprocess.call(["ifconfig", interface, "hw", "ether", new_mac])
-#bring interface up to enable mac address change
-subprocess.call(["ifconfig", interface, "up"])
-
+change_mac(options.interface, options.new_mac)
